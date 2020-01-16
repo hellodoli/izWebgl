db72be8014ed197993cfe0e8ad4d11016abfbe1d
Phat <nmphat0912@gmail.com>
2020-01-17 00:24:14 +0700

    add cubes

--- cubes/index.js
+++ cubes/index.js
@@ -1,3 +1,4 @@
+// define default param for camera, scene, ...
 const param = {
   camera: {
     x: {
@@ -36,38 +37,28 @@ function init () {
   camera.position.y = param.camera.y.default;
   camera.position.z = param.camera.z.default;
 
-  //var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
-  //scene.add( directionalLight );
-
   controls = new THREE.OrbitControls( camera, renderer.domElement );
   controls.update();
-}
-
+};
 
 //
 // custom camera, background, light
 function customBackground () {
   scene.background = new THREE.Color(0x6c757d);
+
   camera.position.z = 40;
   camera.position.y = 10;
+
   var directionalLight = new THREE.DirectionalLight( 0xffffff, .5 );
   scene.add(directionalLight);
-}
 
-let cubes = [];
-const maxCube = 3;
-const PLANE = {
-  width: 50,
-  height: 50,
-  xRange: {
-    min: -(PLANE.width / 2),
-    max: (PLANE.width / 2)
-  },
-  zRange: {
-    min: -(PLANE.height / 2),
-    max: (PLANE.height / 2)
-  }
+  var axesHelper = new THREE.AxesHelper(50);
+  scene.add(axesHelper);
 };
+
+// define default Plane, Cube
+let cubes = []; // list array store cube
+const maxCube = 3;
 const CUBE = {
   x: 10,
   y: 3,
@@ -82,6 +73,21 @@ const CUBE = {
   }
 };
 
+const widthPlane = 55;
+const heightPlane = 50;
+const PLANE = {
+  width: widthPlane,
+  height: heightPlane,
+  xRange: {
+    min: -(widthPlane / 2),
+    max: (widthPlane / 2),
+  },
+  zRange: {
+    min: -(heightPlane / 2),
+    max: (heightPlane / 2)
+  }
+};
+
 function createPlane () {
   var geometry = new THREE.PlaneGeometry( PLANE.width, PLANE.height, (PLANE.width / 2), (PLANE.height / 2) );
   var material = new THREE.MeshBasicMaterial({
@@ -89,25 +95,45 @@ function createPlane () {
     color: 0xffffff,
     wireframe: true
   });
-  var p = new THREE.Mesh(geometry, material);
-  p.rotateX(-Math.PI*0.5);
-  scene.add(p);
+  var plane = new THREE.Mesh(geometry, material);
+  plane.rotateX(-Math.PI*0.5);
+  scene.add(plane);
 };
 
 function createCube () {
-  const geometry = new THREE.BoxGeometry(CUBE.x, CUBE.y, CUBE.z);
-  var material = new THREE.MeshPhongMaterial({
-    side: THREE.DoubleSide,
-    shininess: 100,
-    color: Math.random() * 0xffffff,
-    wireframe: false
-  });
-  var cube = new THREE.Mesh(geometry, material);
-  cube.position.x = (PLANE.xRange.min) - CUBE.x;
-  cube.position.z = 0;
-  cube.position.y = (CUBE.y*0.5); // cube will upper plane
-  cubes.push(cube);
-  scene.add(cube);
+  const maxCubeAtXRound = parseInt(PLANE.width / CUBE.x);
+  const maxCubeAtX =  ((PLANE.width / CUBE.x) > maxCubeAtXRound) ? (maxCubeAtXRound + 1) : maxCubeAtXRound;
+  let i = 0;
+  while (i < maxCubeAtX) {
+    // khoảng cách từ 1 cube đến gốc tọa độ tính từ giữa cube đến gốc tọa độ
+    // chứ không phải từ bên phải (trái) cùng của cube
+    const cubePosX = PLANE.xRange.min + (CUBE.x / 2) + (i*CUBE.x);
+    let cubeX = 0;
+    if ((cubePosX + (CUBE.x / 2)) > PLANE.xRange.max) {
+      // cut cube x
+      cubeX = (CUBE.x / 2) + (PLANE.xRange.max - cubePosX);
+    } else {
+      cubeX = CUBE.x;
+    }
+    
+    const geometry = new THREE.BoxGeometry(cubeX, CUBE.y, CUBE.z);
+    const material = new THREE.MeshPhongMaterial({
+      side: THREE.DoubleSide,
+      shininess: 100,
+      color: Math.random() * 0xffffff,
+      wireframe: false
+    });
+    let cube = new THREE.Mesh(geometry, material);
+    cube.position.z = 0;
+    cube.position.x = (PLANE.xRange.min + (CUBE.x / 2) + (i*CUBE.x));
+    cube.position.y = (CUBE.y / 2); // cube will upper plane
+    if (i === 4) {
+      console.log(cube);
+    }
+    cubes.push(cube);
+    scene.add(cube);
+    i++;
+  }
   
   /*for (let i = 0; i < maxCube; i++) {
     var material = new THREE.MeshPhongMaterial({
@@ -128,7 +154,11 @@ function createCube () {
       // material
       var x = randomRange(-20,20);
       var y
-
     }
   */
+};
+
+function render () {
+  requestAnimationFrame(render);
+  renderer.render(scene, camera);
 };
